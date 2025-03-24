6bf81b934b8d6dd0b479778659e74ba678b24e59
JoSeungBin <67574367+vkflco08@users.noreply.github.com>
2025-03-25 01:30:49 +0900

    [Silver III] Title: N과 M (1), Time: 8 ms, Memory: 3472 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int N, M;
+int arr[11] = {0};
+bool visited[11] = {false};
+string output; 
+
+void algorithm(int index){
+    if(M < index) {
+        for (int i = 1; i <= M; i++) {
+            output += to_string(arr[i]) + " ";
+        }
+        output += "\n";
+    } else {
+        for(int i = 1; i<=N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[index] = i;
+                algorithm(index+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+
+    algorithm(1);
+    cout << output;
+}
