cbec6600b6081756ca9479be26723fcc4de2a7a4
JoSeungBin <67574367+vkflco08@users.noreply.github.com>
2025-03-25 18:28:09 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+int L, C;
+vector<char> vec(16);
+vector<char> tmpArr(16);
+string result;
+
+bool isValid() {
+    int vowel = 0, consonant = 0;
+    for (char c : tmpArr) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') vowel++;
+        else consonant++;
+    }
+    return (vowel >= 1 && consonant >= 2);
+}
+
+void algorithm(int index, int start) {
+    if (index == L) {
+        if (isValid()) {
+            for(int i = 0; i<L; i++){
+                result += tmpArr[i];
+            }
+            result += "\n";
+        }
+        return;
+    }
+
+    for (int i = start; i < C; i++) {
+        tmpArr[index] = vec[i];  
+        algorithm(index + 1, i + 1);  // 다음 문자 선택
+    }
+}
+
+int main() {
+    cin >> L >> C;
+
+    vec.resize(C);
+    tmpArr.resize(L);
+
+    for(int i = 0; i < C; i++){
+        cin >> vec[i];
+    }
+
+    sort(vec.begin(), vec.begin() + C);
+
+    algorithm(0, 0);
+
+    cout << result;
+}
